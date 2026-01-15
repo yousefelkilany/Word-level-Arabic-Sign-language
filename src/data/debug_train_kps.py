36ae7a96dffd7017d9f7b8b1c954df8ff0393286
Youssef Elkilany <youssof.kilaney@gmail.com>
2026-01-15 21:26:42 +0200

    debug

--- src/data/debug_train_kps.py
+++ src/data/debug_train_kps.py
@@ -1,9 +1,10 @@
 import cv2
 
-from data.dataloader import prepare_mmap_dataloaders
+from data.dataloader import prepare_mmap_dataloaders, prepare_lazy_dataloaders
 from data.debug_live_viz import visualize_debug_skeleton
 
-if __name__ == "__main__":
+
+def test_mmap_dataset():
     train_dl, val_dl, test_dl = prepare_mmap_dataloaders()
 
     for kps, labels in train_dl:
@@ -20,3 +21,27 @@ if __name__ == "__main__":
         cv2.imwrite("debug_training_sample.jpg", img)
 
         break
+
+
+def test_lazy_dataset():
+    train_dl, val_dl, test_dl = prepare_lazy_dataloaders(range(2), batch_size=64)
+
+    for kps, labels in train_dl:
+        kps, labels = kps, labels
+        print(f"{kps.shape = }")
+        print(f"{labels.shape = }")
+
+        # sample_frame = kps[0][30].reshape(-1, 3)
+        # print(f"{sample_frame.shape = }")
+
+        # img, report = visualize_debug_skeleton(sample_frame)
+        # print("--- TRAINING DATA STATS ---")
+        # print(report)
+        # cv2.imwrite("debug_training_sample.jpg", img)
+
+        break
+
+
+if __name__ == "__main__":
+    # test_mmap_dataset()
+    test_lazy_dataset()
