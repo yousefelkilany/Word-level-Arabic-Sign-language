daa77e129d0fed64036c7bf32d3cdf61523e6de4
Youssef Elkilany <youssof.kilaney@gmail.com>
2026-01-19 16:21:53 +0200

    debug

--- src/modelling/parallel_train.py
+++ src/modelling/parallel_train.py
@@ -33,7 +33,7 @@ def run_training(rank, world_size):
 
     torch.manual_seed(42)
 
-    num_words = 15
+    num_words = 30
     signers = ["01", "02", "03"]
     selected_words = range(1, num_words + 1)
     batch_size = 64
